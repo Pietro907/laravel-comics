d9c813705bcf70fcd870d5f4a86b6c948a856061
Pietro907 <pietrop.puri@gmail.com>
2023-10-26 16:14:42 +0200

    add img and draft header

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,12 +1,33 @@
-<header id="site_header" class="text-center shadow">
+<header>
+  <div class="container_header">
+    <div class="row jst_btwn">
+      <div class="col-3">
 
-    <nav class="nav justify-content-center">
-        <a class="nav-link active" href="#">H</a>
-        <a class="nav-link" href="#">O</a>
-        <a class="nav-link disable" href="#">Malscajscjsò</a>
-        <a class="nav-link disable" href="#">E</a>
-    </nav>
+        <!-- Logo immagine -->
+        <img class="logo" src="../assets/img/dc-logo.png" alt="logo DC Univers">
 
+      </div>
+
+      <div class="col-9 ">
+
+        <!-- Navbar header loop per 10 -->
+        <nav class="navbar navbar-expand-sm jst_btwn">
+          
+          <div v-for="linkHeader in navHeader" class="container_a pad_x">
+
+            <a class="active navbar-brand text-red text-uppercase" href="#"></a>
+
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+
+
+
+  </div>
 
 
 </header>
